ea0a647a04d3e8ab63f47b7a0147e85c04651b4f
adarsh0error <adarshmittal621@gmail.com>
2026-02-19 08:27:08 +0530

    [Ayush Lakra , Completed UC1]

---
+++ palindrome/src/PalindromeCheck.java
@@ -0,0 +1,8 @@
+public class PalindromeCheck
+    {
+        static void main() {
+            System.out.println("Welcome to palindrome checker management system");
+            System.out.println("version 1.0");
+            System.out.println("System initialized successfully");
+        }
+    }
