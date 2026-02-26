5e5d6a24c3499dfcc6b578e57e7145637da90a39
AyushNischay <al2278@srmist.edu.in>
2026-02-26 09:38:54 +0530

    UC6 done

--- palindrome/src/PalindromeCheck.java
+++ palindrome/src/PalindromeCheck.java
@@ -1,19 +1,22 @@
-import java.util.*;
-public class PalindromeCheck {
-    public static void main(String[] args) {
-        String input = "noon";
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
-        }
-        String reversed = "";
-        while (!stack.isEmpty()) {
-            reversed += stack.pop();
-        }
-        if (input.equals(reversed)) {
-            System.out.println(input+" is a palindrome.");
-        } else {
-            System.out.println(input+" is not a palindrome.");
-        }
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+static void main() {
+    String input = "civic";
+    Queue<Character> queue = new LinkedList<>();
+    Stack<Character> stack = new Stack<>();
+    for (char c : input.toCharArray()) {
+        queue.add(c);
+        stack.push(c);
+    }
+    boolean isPalindrome = true;
+    while (!queue.isEmpty()) {
+        if (!queue.remove().equals(stack.pop())) {
+            isPalindrome = false;
+            break;
         }
     }
+    System.out.println("Input : " + input);
+    System.out.println("Is Palindrome? : " + isPalindrome);
+}
